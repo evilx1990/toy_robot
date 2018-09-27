02d354380a87d9c661c87fb82a65f6d01fef79c3
evilx <sergey.emelyanov@faceit-team.com>
2018-09-28 00:46:14 +0300

    v

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ simulator = SimulatorTR.new
 if ARGV.length.eql?(2)
   simulator.place.create!(ARGV[0].to_i, ARGV[1].to_i)
 else
-  simulator.place.create!(5, 6)
+  simulator.place.create!
 end
 
 puts simulator.place.to_s
